e26af08b825923de066818851e37893bb4d1323c
[youngdae.seo] <miryo9yd@naver.com>
2024-07-03 11:00:12 +0900

    32 Page

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
 #include "../primefactors/prime-factors.cpp"
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+#include <vector>
+#include "test.h"
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
