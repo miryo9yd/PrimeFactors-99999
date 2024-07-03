4df6dee114c2ca0c55c9852ea937c7479f82ce29
[youngdae.seo] <miryo9yd@naver.com>
2024-07-03 11:22:46 +0900

    59 Page

--- primefactors/prime-factors.cpp
+++ primefactors/prime-factors.cpp
@@ -2,26 +2,16 @@
 using namespace std;
 class PrimeFactor {
 public : 
-	int divisor = 2;
+
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1  ) {
-			if (number == 4 ||number == 6|| number == 9|| number ==12) {
-				for (divisor == 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
-
-
-				}
-
-
+		for (int divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				result.push_back(divisor);
+				number /= divisor;
 			}
-			else {
-				result.push_back(number);
-			}
-			
+
+
 		}
 		return result;
 	}
